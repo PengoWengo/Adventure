5f21487a08b77a8f8c2f1c54b08cb0c9d2670089
PengoWengo <113034961+PengoWengo@users.noreply.github.com>
2023-06-21 15:49:14 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+/* Declare variables below to save the different sections (divs) of your story*/
+
+
+
+
+
+
+
+
+
+.open-one-screen.onclick=function(){
+};
+
+// INSERT_VARIABLE.onclick=function(){
+
+// };
+
+
+// INSERT_VARIABLE.onclick=function(){
+
+// };
+
