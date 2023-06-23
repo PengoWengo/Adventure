9d0f76e504233efedb25aef185b59f041dfd11b4
PengoWengo <alexandergillett218@gmail.com>
2023-06-23 10:55:28 -0500

    submitting

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ var optOne2 = document.getElementById("option-one-two")
 var optTwo2 = document.getElementById("option-two-two")
 var optOne3 = document.getElementById("option-one-three")
 var optTwo3 = document.getElementById("option-two-three")
+var optOne4 = document.getElementById("option-one-four")
+var optTwo4 = document.getElementById("option-two-four")
+var screen2 = document.querySelector(".open-one-screen-two")
+var screen3 = document.querySelector(".open-two-end-two")
 var begin = document.getElementById('begin');
 var optOne = document.querySelector(".open-one-screen")
 var optTwo = document.querySelector(".open-two-screen")
@@ -18,8 +22,12 @@ var end1 = document.querySelector(".open-one-end")
 var reset = document.querySelector(".reset")
 var optOne22 = document.getElementById("option-one-two-two")
 var buttons3 = document.querySelector(".buttons3")
+var buttons4 = document.querySelector(".buttons4")
 var eggGone = document.getElementById("eggGone")
-
+var win1 = document.querySelector(".open-one-end-two")
+var winBTN = document.querySelector(".endBTN1")
+var winEgg = document.getElementById("winEgg")
+var endEnd = document.querySelector(".endEnd1")
 
 let one = 0;
 function click1(){
@@ -74,10 +82,32 @@ optTwo2.onclick=function(){
 optOne3.onclick=function(){
     buttons3.style.display = "none"
     optTwo.style.display = "none"
+    screen2.style.display = "block"
+    buttons4.style.display = "block"
 }
 optTwo3.onclick=function(){
     buttons3.style.display = "none"
     optTwo.style.display = "none"
     end1.style.display = "block"
     eggGone.innerHTML = "You let the animal get away with your egg and you have lost your only egg."
+}
+optOne4.onclick=function(){
+    buttons4.style.display = "none"
+    optOne.style.display = "none"
+    win1.style.display = "block"
+    screen2.style.display = "none"
+    winBTN.style.display = "block"
+}
+optTwo4.onclick=function(){
+    buttons4.style.display = "none"
+    optOne.style.display = "none"
+    screen2.style.display = "none"
+    screen3.style.display = "block"
+    winBTN.style.display = "block"
+}
+winEgg.onclick=function(){
+    win1.style.display = "none"
+    winBTN.style.display = "none"
+    endEnd.style.display = "block"
+    screen3.style.display = "none"
 }
